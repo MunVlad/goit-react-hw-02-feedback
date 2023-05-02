6ca7d90288fd0e033e4fb9d554cdf430c18104b4
MunVlad <mvv2006@ukr.net>
2023-05-02 23:50:23 +0300

    step_2_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, {Component} from "react";
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleIncrementStates = state => {
+    this.setState(prevState => {
+      return {
+         ...prevState,
+         [state]: prevState[state] + 1,
+      }
+    });
+  }
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+  
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+  return total === 0 ? 0 : Math.round((good / total) * 100);
+  }
+
+
+  render() {
+    return (<div>
+      <h1>Please leave feedback</h1>
+      <div>
+        <button type='button' onClick ={() => this.handleIncrementStates("good")}>Good</button>
+        <button type='button' onClick ={() => this.handleIncrementStates("neutral")}>Neutral</button>
+        <button type='button' onClick ={() => this.handleIncrementStates("bad")}>Bad</button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good:{this.state.good}</li>
+          <li>Neutral:{this.state.neutral}</li>
+          <li>Bad:{this.state.bad}</li>
+          <li>Total:{this.countTotalFeedback()}</li>
+          <li>Positive feedback:{this.countPositiveFeedbackPercentage()}%</li>
+        </ul>
+      </div>
+</div>)
+  }
+}
+export default App;
